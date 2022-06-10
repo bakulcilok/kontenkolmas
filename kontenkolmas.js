cb7cc680079583ad03df337b8b6dd3597dea37ad
bakulcilok <67635957+bakulcilok@users.noreply.github.com>
2022-06-10 21:55:29 +0700

    Create kontenkolmas.js

---
+++ kontenkolmas.js
@@ -0,0 +1,9 @@
+[{
+    "creator": "Bakulcilok",
+    "number": "1",
+    "konten1": "https://telegra.ph/file/9e04246bc7f296a4b2f1a.jpg",
+    "konten2": "https://telegra.ph/file/a16c1cc4a86033984fef4.jpg"
+    "konten3": "https://telegra.ph/file/b17603a53adf7f7dba3d3.jpg"
+    "konten4": "https://telegra.ph/file/1c04652ad0ab489d84f34.jpg"
+    "konten5": "https://telegra.ph/file/9fadcf99d0bdc9ce1c395.jpg"
+}]
